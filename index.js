728c7dede67bec2f210718e7b65da8e010aebd66
Sess254 <wamaritemoses@gmail.com>
2024-05-16 00:16:56 +0300

    main styling done, on to the javascript section

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase , ref , push , onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://we-are-the-champions-478ef-default-rtdb.asia-southeast1.firebasedatabase.app"
+}
+
+const app = initializeApp(appSettings)
+const database = getDataBase(app)
+const endorsementListInDB = ref(database, "endorsmentList")
+
