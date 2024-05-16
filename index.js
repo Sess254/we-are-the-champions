d3093d2cbb1ba9701a89f280c0c52313db8046bc
Sess254 <wamaritemoses@gmail.com>
2024-05-16 13:57:54 +0300

    button is able to push data into the database

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase , ref , push , onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://we-are-the-champions-478ef-default-rtdb.asia-southeast1.firebasedatabase.app"
+    databaseURL: "https://we-are-the-champions-478ef-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
-const database = getDataBase(app)
-const endorsementListInDB = ref(database, "endorsmentList")
+const database = getDatabase(app)
+const endorsementInDB = ref(database, "endorsments")
 
+const inputFieldEl = document.getElementById("input-field")
+const publishButtonEl = document.getElementById("publish-button")
+
+// publish button event listener
+publishButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(endorsementInDB, inputValue)
+
+})
