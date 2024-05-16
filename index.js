f99d8a3d30b8b250ad641379e67047953a86100e
Sess254 <wamaritemoses@gmail.com>
2024-05-16 18:24:05 +0300

    it now generates endorsements,fetches from and deletes from the database

--- index.js
+++ index.js
@@ -7,14 +7,65 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const endorsementInDB = ref(database, "endorsments")
+const endorsementInDB = ref(database, "endorsements")
 
 const inputFieldEl = document.getElementById("input-field")
 const publishButtonEl = document.getElementById("publish-button")
+const endorsementEl = document.getElementById("endorsement-el")
 
 // publish button event listener
 publishButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(endorsementInDB, inputValue)
-
+    clearInputField()
 })
+
+// function that fetches data from the database
+onValue(endorsementInDB, function(snapshot) {
+     if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+        clearEndorsementList()
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+
+            appendToendorsementList(currentItem)
+        }
+    
+    } else {
+        endorsementEl.innerHTML = `No Endorsements at the Moment....`
+    }
+})
+
+
+// function that clears the input field
+function clearInputField() {
+    inputFieldEl.value = ""
+}
+
+// funtion that appends new endorsements to the endorsements list
+function appendToendorsementList(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    // function deletes endorsements from te database
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationInDB = ref(database, `endorsements/${itemID}`)
+        remove(exactLocationInDB)
+
+    })
+
+    endorsementEl.append(newEl)
+}
+
+//function that clears the endoserments
+function clearEndorsementList() {
+    endorsementEl.innerHTML = ""
+}
